d6708673849436517fbd05bee872348263c2de95
Maja Brankovic <brankovicm30@gmail.com>
2023-02-06 21:09:56 +0100

    Napravljenje migracije za kreiranje tabela

---
+++ laravel_tike/database/migrations/2023_02_06_193456_create_kategorija_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategorija', function (Blueprint $table) {
+            $table->id();
+            $table->string('naziv')->unique();
+            $table->string('url_slike')->unique();
+            $table->text('opis')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategorija');
+    }
+};
